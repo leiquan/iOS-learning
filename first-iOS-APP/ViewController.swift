981f4eb3181c42b23ec2fd8aaccb1f7b25df0a57
leiquan <leiquan@MacBook.local>
2015-09-10 23:50:20 -0700

    Initial Commit

---
+++ first-iOS-APP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  first-iOS-APP
+//
+//  Created by leiquan on 15/9/10.
+//  Copyright (c) 2015年 leiquan.me. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
